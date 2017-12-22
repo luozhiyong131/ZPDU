58954132f6ef5ea2fe48e3a5699264d5de0b4d88
lzy <luozhiyong131@qq.com>
2017-12-22 16:55:49 +0800

    aa

--- DIPS/dips/main.cpp
+++ DIPS/dips/main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
+#include <QtCore>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    qDebug() << "AAAAAAAAAA";
 
     return a.exec();
 }
